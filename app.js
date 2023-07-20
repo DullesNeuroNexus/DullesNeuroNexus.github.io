a5dcf4d296737955750e18fb4dfc6081a6450264
TheQuantumPikachu <TheQuantumPikachu@users.noreply.github.com>
2023-07-20 18:08:48 -0500

    COOL ASS GLOW FX ADDED
    I hate JavaScript but I somehow managed to add a cool ass glow effect to the title div

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-}) 
+alert('app.js imported');
+var titleglow = 0;
 
-const hiddenElem = document.querySelectorAll('.hidden');
-hiddenElem.forEach((el) => observer.observe(el));
+var t = document.getElementById('title')
+
+function repeatWhileMouseOver(element, action, deaction) {
+    var interval = null;
+    element.addEventListener('mouseover', function() {
+        clearInterval(interval);
+        interval = setInterval(action, 30);
+    });
+
+    element.addEventListener('mouseout', function() {
+        clearInterval(interval);
+        interval = setInterval(deaction, 30);
+    });
+}
+
+function titleglowup() {
+    if (titleglow < 10) {titleglow++;}
+    t.style.boxShadow = "0 0 "+String(titleglow)+"px "+String(titleglow>>0)+"px #48abe0";
+    t.style.textShadow = "#48abe0 0 0 "+String(titleglow/5>>0);
+}
+function titleglowdown() {
+    if (titleglow > 0) {titleglow--;}
+    t.style.boxShadow = "0 0 "+String(titleglow)+"px "+String(titleglow>>0)+"px #48abe0";
+    t.style.textShadow = "#48abe0 0 0 "+String(titleglow/5>>0);
+}
+
+repeatWhileMouseOver(t, titleglowup, titleglowdown);
