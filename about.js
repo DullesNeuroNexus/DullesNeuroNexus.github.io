c409b5596fd8924f9409849e510f663674627c89
TheQuantumPikachu <TheQuantumPikachu@users.noreply.github.com>
2023-07-27 23:41:51 -0500

    several changes
    reorganization of media + about.html implemented

--- about.js
+++ about.js
@@ -1,10 +1,10 @@
 console.log('about.js imported');
 
-//For whatever reason whenever I tried to use the fetch API with about.json it would have a seizure and say some dumb BS
-//Thus I have chosen to minify about.json
+//For whatever reason whenever I tried to use the fetch API with the actual about.json file it would have a seizure
+//Thus I have chosen to minify about.json and throw it into the javascript lol
 //I hate JavaScript
 
-const raw = '[{"id":1,"name":"Sean Carawan","role":"Founder & President","desc":"Sean is a rising junior at Dulles. As the club founder and president, Sean is the reason why this club exists, and is excited to share his passion for neuroscience.","mail":"None","linkedin":"https://www.linkedin.com/in/sean-carawan-1ab28a284"},{"id":2,"name":"Musir Merani","role":"Vice President - Education","desc":"Musir is a rising junior at Dulles. As the Vice President of Education, he works with Sean to create an efficient curriculum designed to teach students about neuroscience.","mail":"None","linkedin":"None"},{"id":3,"name":"Ritvik Mangire","role":"Vice President - Operations","desc":"Ritvik is a rising Junior at Dulles. Ritvik works a vital part of this club as he foresees all club operations & finances.","mail":"ritvikmangire@hotmail.com","linkedin":"https://www.linkedin.com/in/ritvik-mangire-00b93526b"},{"id":4,"name":"Nidhi Vankadaru","role":"Secretary","desc":"Nidhi is a rising Junior at Dulles.","mail":"None","linkedin":"None"},{"id":5,"name":"Allen Jacob","role":"Secretary","desc":"Allen is a rising Junior at Dulles.","mail":"None","linkedin":"None"},{"id":6,"name":"Miraj Parikh","role":"Marketing Director & Web Developer","desc":"Miraj is a rising Sophomore at Dulles.","mail":"None","linkedin":"None"},{"id":7,"name":"Natalie Lam","role":"Event Coordinator","desc":"Natalie is a rising Junior at Dulles.","mail":"None","linkedin":"None"},{"id":8,"name":"Sammith Krishna","role":"Communications Officer","desc":"Sammith is a rising Junior at Dulles.","mail":"None","linkedin":"None"},{"id":4,"name":"Nakshatra (Nikki) Kothapalli","role":"Student Life Lead","desc":"Nidhi is a rising Sophomore at Dulles.","mail":"None","linkedin":"None"}]'
+const raw = '[{"name":"Sean Carawan","role":"Founder & President","desc":"Sean is a rising junior at Dulles. As the club founder and president, Sean is the reason why this club exists, and is excited to share his passion for neuroscience.","mail":false,"linkedin":"https://www.linkedin.com/in/sean-carawan-1ab28a284","pic":"seanC.jpg"},{"name":"Musir Merani","role":"Vice President - Education","desc":"Musir is a rising junior at Dulles. As the Vice President of Education, he works with Sean to create an efficient curriculum designed to teach students about neuroscience.","mail":false,"linkedin":false,"pic":"musirM.jpg"},{"name":"Ritvik Mangire","role":"Vice President - Operations","desc":"Ritvik is a rising Junior at Dulles. Ritvik works a vital part of this club as he foresees all club operations & finances.","mail":"ritvikmangire@hotmail.com","linkedin":"https://www.linkedin.com/in/ritvik-mangire-00b93526b","pic":"ritvikM.jpg"},{"name":"Nidhi Vankadaru","role":"Secretary","desc":"Nidhi is a rising Junior at Dulles.","mail":false,"linkedin":false,"pic":"nidhiV.jpg"},{"name":"Allen Jacob","role":"Secretary","desc":"Allen is a rising Junior at Dulles.","mail":false,"linkedin":false,"pic":"allenJ.jpg"},{"name":"Miraj Parikh","role":"Marketing Director & Web Developer","desc":"Miraj is a rising Sophomore at Dulles.","mail":false,"linkedin":false,"pic":false},{"name":"Natalie Lam","role":"Event Coordinator","desc":"Natalie is a rising Junior at Dulles.","mail":false,"linkedin":false,"pic":"natalieL.jpg"},{"name":"Sammith Krishna","role":"Communications Officer","desc":"Sammith is a rising Junior at Dulles.","mail":false,"linkedin":false,"pic":"sammithK.jpg"},{"name":"Nakshatra (Nikki) Kothapalli","role":"Student Life Lead","desc":"Nidhi is a rising Sophomore at Dulles.","mail":false,"linkedin":false,"pic":"nikkiK.jpg"}]'
 const members = JSON.parse(raw);
 
 console.log('const raw parsed into const members');
@@ -15,12 +15,18 @@ let out = "";
 console.log('placeholder constructed');
 
 for(let member of members){
+    
     out += `
     <div class="gridtext-div">
         <h3 class="gridtext-role">${member.role}</h3>
         <h2 class="gridtext-name">${member.name}</h2>
-    </div>
+        <p class="gridtext-desc">${member.desc}</p>
     `;
+    
+    if (member.pic) out += `<img class="gridtext-img" src="media/officers/${member.pic}" alt="A photo of ${member.name}">`;
+    
+    out += `
+    </div>`;
 }
 console.log('out constructed');
 
